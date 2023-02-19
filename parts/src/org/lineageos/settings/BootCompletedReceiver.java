72d6a28c4777baba6cf8430211d46207a1a6590a
Ghosuto <clash.raja10@gmail.com>
2023-02-19 06:54:43 +0100

    sm8250-common:  [squash] : Implement HBM & Auto HBM
    sm8250-common: parts: Implement HBM & Auto HBM
    sm8250-common: Set HBM permissions
    sm8250-common: Disable hbm thermal throttle
    sm8250-common: sepolicy: Allow devicesettings write to vendor_sysfs_graphics
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: EmanuelCN <emanuelgithub@gmail.com>
    Co-Authored-By: Max Weninger <max.weninger@gmail.com>
    Co-Authored-By: AnierinB <anierin@evolution-x.org>
    Co-Authored-By: EmanuelCN <emanuelgithub@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -27,7 +27,7 @@ import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.touchsampling.TouchSamplingUtils;
-
+import org.lineageos.settings.utils.FileUtils;
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
@@ -45,5 +45,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         ThermalUtils.startService(context);
         RefreshUtils.startService(context);
         TouchSamplingUtils.restoreSamplingValue(context);
+        FileUtils.enableService(context);
     }
 }
